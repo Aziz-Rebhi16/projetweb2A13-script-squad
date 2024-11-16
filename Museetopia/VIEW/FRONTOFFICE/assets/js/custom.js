f0bfd1ad018b6589627ac857ba59db0200c75fec
khalil rebhi <aziz.rebhi@esprit.tn>
2024-11-16 01:48:25 +0100

    16/11/24

--- Museetopia/VIEW/FRONTOFFICE/assets/js/custom.js
+++ Museetopia/VIEW/FRONTOFFICE/assets/js/custom.js
@@ -13,7 +13,7 @@
           slides: [
               { src: 'images/slides/sincere-laugh-showing-picture-smartphone-casual-meeting-with-best-friends-restaurant-terrace.jpg' },
               { src: 'images/happy-waitress-giving-coffee-customers-while-serving-them-coffee-shop.jpg' },
-              { src: 'images/young-female-barista-wear-face-mask-serving-take-away-hot-coffee-paper-cup-consumer-cafe.jpg' }
+              { src: 'images/Muse_bardo_tunis.jpg' }
           ],
           timer: false,
           animation: 'kenburns',
